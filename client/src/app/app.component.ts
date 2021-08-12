439e3b855436eab0fff8a90d6e25ecd1bc198c9a
singhgyan72 <singhgyan72@gmail.com>
2021-08-12 15:13:58 +0530

    Section 5: Client login and register

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -8,20 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   users: any;
-  constructor(private http: HttpClient) {
+  constructor(private accountService: AccountService) {
 
   }
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('http://localhost:5000/api/users').subscribe(
-      data => {
-        this.users = data;
-      }, error => {
-        console.log(error);
-      }
-    );
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user') || '{}');
+    this.accountService.setCurrentUser(user);
   }
 }
